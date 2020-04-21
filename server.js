4ea33572e7443ea5636effc9258e1bee13872cc5
rzamperlini <rzamperlini@gmail.com>
2020-04-21 19:15:12 -0300

    Instalação e utilização do Nodemon

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(req,res)=>{res.send('Hello Rocketseat')});
+app.get('/',(req,res)=>{res.send('Hello World')});
 
 app.listen(3001);
 
