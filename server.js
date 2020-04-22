bb1a267a57b7b23bb9f76b568fd0549c469035da
rzamperlini <rzamperlini@gmail.com>
2020-04-21 21:17:11 -0300

    Conectando com o Mongo Database

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//Iniciando o App
 const app = express();
 
+//Iniciando o Database
+mongoose.connect('mongodb://localhost:27017/nodeapi', {useUnifiedTopology:true, useNewUrlParser:true});
+
+
+//Primeira rota
 app.get('/',(req,res)=>{res.send('Hello World')});
 
 app.listen(3001);
