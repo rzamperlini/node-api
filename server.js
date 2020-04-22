5f7282207815d19f64b3cbe2622ad7bfb2545220
rzamperlini <rzamperlini@gmail.com>
2020-04-21 21:43:24 -0300

    Criação do Model de Produto - concluído

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 //Iniciando o App
 const app = express();
@@ -7,9 +8,21 @@ const app = express();
 //Iniciando o Database
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useUnifiedTopology:true, useNewUrlParser:true});
 
+requireDir("./src/models");
+
+const Product = mongoose.model('Product');
 
 //Primeira rota
-app.get('/',(req,res)=>{res.send('Hello World')});
+app.get('/',(req,res)=>{ 
+
+    Product.create({
+        title:'React-Native',
+        description: 'Build native apps with React',
+        url:'http://github.com/facebook/react-native'
+    });
+
+    return res.send('Hello World')
+});
 
 app.listen(3001);
 
